25cbd70b36b5b85f3b8390bff5a98fcef5a8c7bd
Willow (GHOST) <git@willow.sh>
2026-02-21 20:22:27 +0000

    refactor: split up input component

--- src/routes/chat/[id]/+page.svelte
+++ src/routes/chat/[id]/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import Input, { type Option } from '$lib/chat/input/Input.svelte';
 	import { AccountSchema, ChatSchema } from '$lib/state/db.svelte';
+	import ModelSelect, { type Option } from './ModelSelect.svelte';
 	import { Providers } from '$lib/state/providers.svelte';
+	import Editor from '$lib/chat/input/Editor.svelte';
 	import { AccountCoState } from 'jazz-tools/svelte';
+	import IconArrowUp from '~icons/lucide/arrow-up';
 	import { Chat } from '$lib/state/chats.svelte';
 	import { tick, untrack } from 'svelte';
 	import { goto } from '$app/navigation';
@@ -37,7 +39,48 @@
 				: untrack(() => new Chat(params.id, providers));
 	});
 
-	let selected = $state<Option>();
+	let selected = $derived<Option | undefined>(
+		chat?.providerId && chat.modelId
+			? options.find(
+					(o) =>
+						o.providerId === chat?.providerId &&
+						o.model.id === chat?.modelId,
+				)
+			: undefined,
+	);
+
+	let disabled = $state(false);
+	let input = $state('');
+
+	async function submit() {
+		disabled = true;
+
+		if (!chat) {
+			if (!account.current.$isLoaded || !account.current.root.$isLoaded) {
+				console.log('account not loaded');
+				disabled = false;
+				return;
+			}
+
+			const c = ChatSchema.create({
+				providerId: selected?.providerId ?? null,
+				modelId: selected?.model.id ?? null,
+			});
+
+			account.current.root.chats.$jazz.push(c);
+			const id = c.$jazz.id;
+
+			await goto(resolve('/chat/[id]', { id }), {
+				replaceState: true,
+			});
+
+			await tick();
+		}
+
+		await chat!.sendMessage({ text: input });
+		disabled = false;
+		input = '';
+	}
 </script>
 
 <div class="chat">
@@ -63,48 +106,31 @@
 		{/if}
 	</div>
 
-	<Input
-		{options}
-		bind:selected={
-			() => selected,
-			(value) => {
-				selected = value;
+	<div class="input">
+		<Editor bind:value={input} onSubmit={submit} {disabled} />
 
-				if (chat) {
-					chat.providerId = selected?.providerId ?? null;
-					chat.modelId = selected?.model.id ?? null;
+		<div class="controls">
+			<ModelSelect
+				{options}
+				{disabled}
+				bind:selected={
+					() => selected,
+					(value) => {
+						selected = value;
+
+						if (chat) {
+							chat.providerId = selected?.providerId ?? null;
+							chat.modelId = selected?.model.id ?? null;
+						}
+					}
 				}
-			}
-		}
-		onSubmit={async (input) => {
-			if (!chat) {
-				if (
-					!account.current.$isLoaded ||
-					!account.current.root.$isLoaded
-				) {
-					console.log('account not loaded');
-					return false;
-				}
+			/>
 
-				const c = ChatSchema.create({
-					providerId: selected?.providerId ?? null,
-					modelId: selected?.model.id ?? null,
-				});
-
-				account.current.root.chats.$jazz.push(c);
-				const id = c.$jazz.id;
-
-				await goto(resolve('/chat/[id]', { id }), {
-					replaceState: true,
-				});
-
-				await tick();
-			}
-
-			await chat!.sendMessage({ text: input });
-			return true;
-		}}
-	/>
+			<button class="outline" onclick={submit} {disabled}>
+				<IconArrowUp />
+			</button>
+		</div>
+	</div>
 </div>
 
 <style>
@@ -138,6 +164,26 @@
 					&.error {
 						color: var(--red);
 					}
+				}
+			}
+		}
+
+		.input {
+			display: flex;
+			flex-direction: column;
+			border-top: 2px solid var(--background-secondary);
+			padding: 6px;
+
+			.controls {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+
+				button {
+					display: grid;
+					place-items: center;
+					font-size: 0.95rem;
+					padding: 4px;
 				}
 			}
 		}
